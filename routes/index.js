9248cccc864fc72f8747ed0910b160d828da5cad
William Rivera <william.pr07@hotmail.com>
2019-04-21 19:36:12 -0400

    add request user data to get username

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var router = express.Router();
 
+// Request user data
+router.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { pageTitle: 'NEMP Login' });
+  res.render('index', { title: 'NEMP Login' });
 });
 
 module.exports = router;
